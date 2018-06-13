749a96bd3014bdf8f6d275d85f8302e9a99b100f
Andrew Burrell <saruto@umich.edu>
2018-06-13 11:26:58 -0400

    testing react native tools

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import FingerprintScanner from "react-native-fingerprint-scanner";
 //import RNCamera from "react-native-camera";
 
 
 // Class that represents various state information for the app.
-class AppInto {
+class AppInfo {
     constructor(){
         // Current state of the app, represented with a pesudo-enum
         this.StateEnum = Object.freeze({  })
@@ -26,6 +26,21 @@ class AppInto {
 
 // Main App Class
 export default class App extends Component {
+    // Constructor
+    constructor(props){
+        super(props)
+
+        // Add global state stuff here.
+
+    }
+
+    // Can add functions here for use as callback functions
+    _onPressButton(buttonNum){
+        return () => {
+            Alert.alert("hiya, you pressed button number " + buttonNum + "!");
+        }
+    }
+
     render() {
         return (
             // Outermost view, don't have anything outside of this
@@ -37,13 +52,16 @@ export default class App extends Component {
                 </View>
 
                 {/* Body */}
-                <View style={styles.container}>
-                    <Text>Open up App.js to start working on your app!</Text>
-                    <Text>Changes you make will automatically reload.</Text>
-                    <Text>Shake your phone to open the developer menu.</Text>
-                    <Text>Test text 12345 xDDD.</Text>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>International Leader in Data Stewardship</Text>
+                    <Text style={styles.text}>10,000 studies, comprising of 4.8 million variables</Text>
+                    <Text style={styles.text}>Data Stewardship and Social Science Research Projects</Text>
+                    <Text style={styles.text}>776 member institutions</Text>
                 </View>
 
+                {/* Buttons */}
+                <Button onPress={this._onPressButton(1)} title="Press Me 1"/>
+                <Button onPress={this._onPressButton(2)} title="Press Me 2" color="red"/>
 
             </View>
         );
@@ -52,19 +70,24 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     titleView: {
-        //backgroundColor: "red",
+        backgroundColor: "teal",
         alignItems: "center",
         justifyContent: "center",
     },
     title: {
         fontSize: 100,
         fontWeight: 'bold',
-        color: "green",
+        color: "white",
     },
-    container: {
+    textContainer: {
         flex: 1,
-        backgroundColor: "blue",
+        backgroundColor: "#005050",
         alignItems: "center",
         justifyContent: "center",
+    },
+    text: {
+        fontSize: 20,
+        color: "white",
+        fontWeight: 'bold',
     },
 });
